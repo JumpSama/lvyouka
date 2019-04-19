30eac683d0dd19ec3cd5c3e677c2944021fcb2be
JumpSama <jumpsama@163.com>
2019-04-19 08:40:57 +0800

    过期处理

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,10 @@
 
 namespace App\Console;
 
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,8 +26,12 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->call(function () {
+            $today = Carbon::today()->toDateString();
+            // 过期处理
+            DB::table('members')->where('status', 1)->where('overdue', '<', $today)
+                ->update(['status' => 0]);
+        })->dailyAt('00:10');
     }
 
     /**
